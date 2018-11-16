9bd6f32d5d21be2f94678d0848b3897f05213e25
bro-seph <josephscottangelus@gmail.com>
2018-11-16 15:48:59 +0100

    Add User model class

--- src/pl/coderslab/warsztat2krks05/Main.java
+++ src/pl/coderslab/warsztat2krks05/Main.java
@@ -1,5 +1,6 @@
 package pl.coderslab.warsztat2krks05;
 
+import pl.coderslab.warsztat2krks05.model.User;
 import pl.coderslab.warsztat2krks05.utils.DbUtils;
 
 import java.sql.Connection;
@@ -8,11 +9,11 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
+
+
         try {
             Connection conn = DbUtils.getConnection();
-
             System.out.println("działa!" + conn.hashCode());
-
             conn.close();
 
         } catch (SQLException e) {
